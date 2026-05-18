6b5721192fb761ea2ec0c13a9d5ce0f8732bdd1a
diegowar2525 <diegowar2525@gmail.com>
2026-05-18 16:27:22 -0500

    Pequeña corrección

--- apps/recomendaciones/admin.py
+++ apps/recomendaciones/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Consulta, Recomendacion
+from .models import Consulta, ResultadoConsulta
 
 # Register your models here.
 admin.site.register(Consulta)
-admin.site.register(Recomendacion)
+admin.site.register(ResultadoConsulta)
