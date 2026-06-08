82ec0a671345acd36bdf5ad7d9898ead8c66e81f
diegowar2525 <diegowar2525@gmail.com>
2026-06-08 16:54:32 -0500

    Remodelación de los modelos de la app usuarios

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -2,11 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+class Rol(models.Model):
+    nombre = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+
 class Usuario(AbstractUser):
-    ROLES = (
-        ("admin", "Administrador"),
-        ("usuario", "Usuario"),
-    )
     GENERO_CHOICES = (
         ("M", "Masculino"),
         ("F", "Femenino"),
@@ -16,18 +19,17 @@ class Usuario(AbstractUser):
     cedula = models.CharField(max_length=10, unique=True, blank=True, null=True)
     nombres = models.CharField(max_length=100, blank=True, default="")
     apellidos = models.CharField(max_length=100, blank=True, default="")
-    genero = models.CharField(
-        max_length=1, choices=GENERO_CHOICES, blank=True, default=""
-    )
+    genero = models.CharField(max_length=1, choices=GENERO_CHOICES, blank=True)
     fecha_nacimiento = models.DateField(blank=True, null=True)
-    es_agronomo = models.BooleanField(default=False)
     profesion = models.CharField(max_length=120, blank=True, default="")
 
-    rol = models.CharField(max_length=20, choices=ROLES, default="usuario")
-    fecha_registro = models.DateTimeField(auto_now_add=True)
+    rol = models.ForeignKey(
+        Rol,
+        on_delete=models.PROTECT,
+        related_name="usuarios"
+    )
 
-    def __str__(self):
-        return f"{self.username} — {self.nombres} {self.apellidos}"
+    fecha_registro = models.DateTimeField(auto_now_add=True)
 
     def nombre_completo(self):
         return f"{self.nombres} {self.apellidos}".strip()
