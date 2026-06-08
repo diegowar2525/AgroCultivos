85e0844ccda88d11ab4720c24fe5dbe79c87cedb
diegowar2525 <diegowar2525@gmail.com>
2026-06-08 17:48:30 -0500

    Registro de modelos en el admin de django

--- apps/cultivos/admin.py
+++ apps/cultivos/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from .models import Categoria, Requerimiento, Cultivo
+from .models import (
+    Categoria,
+    Requerimiento,
+    Cultivo,
+    Estado,
+    CultivoUsuario,
+    TipoSuelo,
+    CultivoTipoSuelo,
+    Amenaza,
+    AmenazaCultivo,
+    TipoAmenaza,
+    SeguimientoCultivo,
+)
 
 # Register your models here.
 admin.site.register(Categoria)
 admin.site.register(Requerimiento)
 admin.site.register(Cultivo)
+admin.site.register(Estado)
+admin.site.register(CultivoUsuario)
+admin.site.register(TipoSuelo)
+admin.site.register(CultivoTipoSuelo)
+admin.site.register(Amenaza)
+admin.site.register(AmenazaCultivo)
+admin.site.register(TipoAmenaza)
+admin.site.register(SeguimientoCultivo)
--- apps/recomendaciones/admin.py
+++ apps/recomendaciones/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Consulta, ResultadoConsulta
+from .models import Consulta, ResultadoConsulta, TipoConsulta
 
 # Register your models here.
 admin.site.register(Consulta)
 admin.site.register(ResultadoConsulta)
+admin.site.register(TipoConsulta)
