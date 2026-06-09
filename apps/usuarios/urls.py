61a685e3286f2bc27df6637416dd9312c30c1ac0
diegowar2525 <diegowar2525@gmail.com>
2026-06-08 19:49:19 -0500

    Problemas residuales por eliminar el modelo rol resueltos

--- apps/usuarios/urls.py
+++ apps/usuarios/urls.py
@@ -1,22 +1,13 @@
 from django.urls import include, path
 
-from rest_framework.routers import DefaultRouter
-
 from .views import (
     RegistroView,
     LoginView,
     PerfilView,
-    RolViewSet,
 )
-
-router = DefaultRouter()
-router.register("roles", RolViewSet)
 
 urlpatterns = [
     path("register/", RegistroView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("profile/", PerfilView.as_view(), name="profile"),
-    path("", include(router.urls)),
 ]
-
-# Ejemplo de ruta completa de register: /api/usuarios/register/ o localhost:8000/api/usuarios/register/
