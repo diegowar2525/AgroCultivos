00ee24fad74897cbd9fcadbfcc154c4301ea08e5
diegowar2525 <diegowar2525@gmail.com>
2026-07-02 22:20:44 -0500

    Componentes obsoletos descartados

--- client/src/components/misCultivos/DataRow.jsx
+++
@@ -1,8 +0,0 @@
-export default function DataRow({ label, value }) {
-    return (
-        <div className="data-row">
-            <span className="data-row-label">{label}</span>
-            <span className="data-row-value">{value}</span>
-        </div>
-    );
-}
