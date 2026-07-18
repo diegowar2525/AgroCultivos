c6518e97fcb7d31358b125238549a5e39228e994
diegowar2525 <diegowar2525@gmail.com>
2026-07-18 15:54:50 -0500

    Componente reubicado y renombrado.

--- client/src/pages/UserDashboard.jsx
+++ client/src/pages/UserDashboard.jsx
@@ -1,7 +1,7 @@
 import Navbar from '../components/layout/Navbar';
 import UserDashboardPanel from '../components/userDashboard/UserDashboardPanel';
 
-export default function Dashboard() {
+export default function UserDashboard() {
     return (
         <div className="dashboard-user-page">
             <Navbar />
